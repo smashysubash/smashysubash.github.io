cbca52596745ad137f7bde6400c1ed22846d6885
Subash S <rocksubash2003@gmail.com>
2025-05-25 12:14:28 +0530

    update path

--- portfolio-v3/next.config.ts
+++ portfolio-v3/next.config.ts
@@ -2,7 +2,9 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-    eslint: {
+  basePath: '/v3',
+  assetPrefix: '/v3/',
+  eslint: {
     ignoreDuringBuilds: true,
   },
   output: 'export',
